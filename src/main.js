4520c225461129bae34f11d032671beaa916b13e
Cesar Martinez <cesar_martinez22@hotmail.com>
2019-05-11 03:08:22 -0300

    set axios defaults

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Axios from 'axios';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+// Axios Base URL
+Axios.defaults.baseURL = 'https://www.googleapis.com/books/v1/';
+
+// Query Param KEY, required for Google Book API
+Axios.defaults.params = {};
+Axios.defaults.params.key = 'AIzaSyDcEOfVnOfuh0KCFRIx1BucqhO7mJKBhjg';
 
 new Vue({
   render: h => h(App),
